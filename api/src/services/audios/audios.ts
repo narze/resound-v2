20588e945b0a42131ff90f19cd12731211b58353
Manassarn Manoonchai <manassarn@gmail.com>
2022-06-29 23:23:02 +0700

    feat: delete audio file from filestack

--- api/src/services/audios/audios.ts
+++ api/src/services/audios/audios.ts
@@ -1,3 +1,4 @@
+import * as Filestack from 'filestack-js'
 import type { QueryResolvers, MutationResolvers } from 'types/graphql'
 
 import { db } from 'src/lib/db'
@@ -28,8 +29,25 @@ export const updateAudio: MutationResolvers['updateAudio'] = ({
   })
 }
 
-export const deleteAudio: MutationResolvers['deleteAudio'] = ({ id }) => {
-  return db.audio.delete({
-    where: { id },
-  })
+export const deleteAudio: MutationResolvers['deleteAudio'] = async ({ id }) => {
+  const client = Filestack.init(process.env.REDWOOD_ENV_FILESTACK_API_KEY)
+
+  const audio = await db.audio.findUnique({ where: { id } })
+
+  // The `security.handle` is the unique part of the Filestack file's url.
+  const handle = audio.url.split('/').pop()
+
+  const security = Filestack.getSecurity(
+    {
+      // We set `expiry` at `now() + 5 minutes`.
+      expiry: new Date().getTime() + 5 * 60 * 1000,
+      handle,
+      call: ['remove'],
+    },
+    process.env.REDWOOD_ENV_FILESTACK_SECRET
+  )
+
+  await client.remove(handle, security)
+
+  return db.audio.delete({ where: { id } })
 }
